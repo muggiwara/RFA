fba5c31354bc8ae9561bd1c8dbc932851955d7bb
muggiwara <OPCmuggiwara@gmail.com>
2014-10-27 22:45:54 +0100

    deleye

--- RFA/RFA.Persistence/Online Scripts/RFA_relations_drop.sql
+++
@@ -1,3 +0,0 @@
-﻿/* CodeFluent Generated Friday, 17 October 2014 17:13. TargetVersion:Sql2008, Sql2012, Sql2014, SqlAzure. Culture:fr-FR. UiCulture:fr-FR. Encoding:utf-8 (http://www.softfluent.com) */
-set quoted_identifier OFF
-GO
